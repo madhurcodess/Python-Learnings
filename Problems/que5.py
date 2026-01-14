7c4b73158075a4359022f0244a3eeb01e2ac596d
madhurcodess <madhurvibes@gmail.com>
2026-01-14 18:07:41 +0530

    Problem Que 5 solved

---
+++ Problems/que5.py
@@ -0,0 +1,27 @@
+# Entered Character is Consonant or Vowels 
+
+x = str(input("Enter Character:"))
+
+if x == "A":
+    print("This is vowel")
+elif x == "a":
+    print("This is vowel")
+elif x == "E":
+    print("This is vowel")
+elif x == "e":
+    print("This is vowel")
+elif x == "I":
+    print("This is vowel")
+elif x == "i":
+    print("This is vowel")
+elif x == "O":
+    print("This is vowel")
+elif x == "o":
+    print("This is vowel")
+elif x == "U":
+    print("This is vowel")
+elif x == "u":
+    print("This is vowel")
+else:
+    print("This is Consonant" )
+
